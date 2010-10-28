37f0729a8dd71e63af8d106472b8515f1a4e17b7
Soeren Sonnenburg <sonne@debian.org>
2010-10-28 16:27:23 +0000

    speedup and simplify libsvm read code

--- ml2h5/converter/h5_libsvm.py
+++ ml2h5/converter/h5_libsvm.py
@@ -10,13 +10,10 @@ class H5_LibSVM(BaseHandler):
     def __init__(self, *args, **kwargs):
         """Constructor.
 
-        @ivar label_maxidx: highest index for a label
-        @type label_maxidx: integer
         @ivar is_multilabel: if data is of type multilabel
         @type is_multilabel: boolean
         """
         super(H5_LibSVM, self).__init__(*args, **kwargs)
-        self.label_maxidx = 0
         self.is_multilabel = False
 
     #def convert_sparse(self, spmatrix):
@@ -28,144 +25,69 @@ class H5_LibSVM(BaseHandler):
     #    else: # dense
     #        data['data'] = A.todense()
 
-    def _scrub_labels(self, label):
-        """Convert labels to doubles and determine max index
+    def get_comment(self):
+        return 'LibSVM'
 
-        @param label: labels read from data file
-        @type label: list of characters
-        @return: labels converted to double
-        @rtype: list of integer
-        """
-        str_label = ''.join(label)
-        if not self.is_multilabel and str_label.find(',') == -1:
-            self.label_maxidx = 0
-            return [numpy.double(str_label)]
-        else:
-            self.is_multilabel = True
-            lab = str_label.split(',')
-            for i in xrange(len(lab)):
-                if not lab[i]:
-                    lab[i] = 0
-                else:
-                    # int conversion to prevent error msg
-                    lab[i] = int(float((lab[i])))
-                if lab[i] > self.label_maxidx:
-                    self.label_maxidx = lab[i]
-            return lab
-
-
-    def _parse_line(self, line):
-        """Parse a LibSVM input line and return attributes.
-
-        @param line: line to parse
-        @type line: string
-        @return: variables in this line
-        @rtype: list of variables
-        """
-        state = 'label'
-        idx = []
-        val = []
-        label = []
-        variables = []
-        for c in line:
-            if state == 'label':
-                if c.isspace():
-                    state = 'idx'
-                    label = self._scrub_labels(label)
-                else:
-                    label.append(c)
-            elif state == 'idx':
-                if not c.isspace():
-                    if c == ':':
-                        state = 'preval'
-                    else:
-                        idx.append(c)
-            elif state == 'preval':
-                if not c.isspace():
-                    val.append(c)
-                    state = 'val'
-            elif state == 'val':
-                if c.isspace():
-                    variables.append([int(''.join(idx)), ''.join(val)])
-                    idx = []
-                    val = []
-                    state = 'idx'
-                else:
-                    val.append(c)
-
-        #import pdb
-        #pdb.set_trace()
-        return {'label':label, 'variables':variables}
-
-
-    def _get_parsed_data(self):
+    def read(self):
         """Retrieves a SciPy Compressed Sparse Column matrix and labels from file.
-
         @return: compressed sparse column matrix + labels
         @rtype: list of scipy.sparse.csc_matrix and label tuple/2-d tuple (multilabel)
         """
-        parsed = []
-        infile = open(self.fname, 'r')
-        for line in infile:
-            parsed.append(self._parse_line(line))
-        infile.close()
-
+        print "start"
         indices_var = []
-        indices_lab = []
         indptr_var = [0]
-        indptr_lab = [0]
         ptr_var = 0
-        ptr_lab = 0
         data_var = []
+
+        indices_lab = []
+        indptr_lab = [0]
+        ptr_lab = 0
         data_lab = []
+
         label = []
-        for i in xrange(len(parsed)):
-            if self.is_multilabel: # -> values are indices
-                for idx in parsed[i]['label']:
+
+        for line in file(self.fname).xreadlines():
+            items = line.split()
+            lab=items[0]
+            dat=items[1:]
+
+            lab=[int(i) for i in lab.split(',')]
+
+            if len(lab)>1:
+                self.is_multilabel = True
+
+            if self.is_multilabel:
+                for idx in lab:
                     indices_lab.append(int(idx))
                     data_lab.append(1.)
                     ptr_lab += 1
                 indptr_lab.append(ptr_lab)
             else: # only single label -> value is actual value
-                label.append(parsed[i]['label'][0])
+                label.append(lab[0])
 
-            for v in parsed[i]['variables']:
-                indices_var.append(int(v[0]) - 1) # -1: (multi)label idx
+            for d in dat:
+                v=d.split(':')
+                indices_var.append(int(v[0]) - 1)
                 data_var.append(numpy.double(v[1]))
                 ptr_var += 1
             indptr_var.append(ptr_var)
 
         if self.is_multilabel:
-            label = csc_matrix(
-                (numpy.array(data_lab), numpy.array(indices_lab), numpy.array(indptr_lab))
-            ).todense()
+            label = csc_matrix( (numpy.array(data_lab), numpy.array(indices_lab),
+                    numpy.array(indptr_lab)) )
         else:
             label = numpy.array(label)
 
-        import pdb
-        pdb.set_trace()
-        return (
-            csc_matrix(
-                (numpy.array(data_var), numpy.array(indices_var), numpy.array(indptr_var))
-            ),
-            label,
-        )
+        data = csc_matrix( (numpy.array(data_var), numpy.array(indices_var),
+            numpy.array(indptr_var)) )
 
-
-    def get_comment(self):
-        return 'LibSVM'
-
-
-    def read(self):
-        (A, label) = self._get_parsed_data()
-        data = { 'label' : label, 'data' : A}
-
+        print "done"
         return {
             'name': self.get_name(),
             'comment': 'LibSVM',
             'ordering': ['label', 'data'],
             'names': ['label', 'data'],
-            'data': data,
+            'data': { 'label' : label, 'data' : data}
         }
 
 
