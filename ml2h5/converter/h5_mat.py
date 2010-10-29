e5b779cdfb271cf44dad225f252a0f4ec5848cb2
Soeren Sonnenburg <sonne@debian.org>
2010-10-29 08:50:00 +0000

    support for reading / writing matlab files with dense matrices/vectors, sparse matrices and cell arrays of strings

--- ml2h5/converter/h5_mat.py
+++ ml2h5/converter/h5_mat.py
@@ -11,9 +11,21 @@ class H5_MAT(BaseHandler):
 
 
     def read(self):
-        matf = loadmat(self.fname, matlab_compatible=True)
-        if matf.has_key('__globals__'):
-            del matf['__globals__']
+        matf = loadmat(self.fname,
+                squeeze_me=False,
+                chars_as_strings=True,
+                mat_dtype=True,
+                struct_as_record=True)
+
+        for k in ('__header__', '__globals__', '__version__'):
+            if matf.has_key(k):
+                del matf[k]
+
+        for k in matf.keys():
+            if matf[k].dtype == numpy.object: # asume cell of strings
+                cell = [unicode(i[0]) for i in matf[k][0]]
+                matf[k]=cell
+
         data = matf
         ordering = matf.keys()
 
@@ -27,4 +39,22 @@ class H5_MAT(BaseHandler):
 
 
     def write(self, data):
-        savemat(self.fname, data['data'], appendmat=False)
+        d=data['data']
+
+        for k in d.keys():
+            if type(d[k])==list and len(d[k])>0 and type(d[k][0])==str:
+                cell = array([ numpy.array(unicode(i)) for i in d[k] ], dtype=numpy.object)
+                d[k] = cell
+            elif type(d[k])==numpy.ndarray and len(d[k])>0 and d[k][0].dtype == numpy.object:
+                cell = numpy.empty((1,len(d[k])), dtype=numpy.object)
+                for i in xrange(len(d[k])):
+                    cell[0,i]=numpy.array(unicode(d[k][i]), dtype='U')
+                d[k] = cell
+
+
+
+        savemat(self.fname, d,
+                appendmat=False,
+                oned_as='row',
+                format='5',
+                long_field_names=True)
