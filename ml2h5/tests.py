4763d4199ee674e79b2324d4bc2ef4d0b4ba87e0
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2011-08-26 11:25:18 +0200

    Test for UCI added and minor bug removed. However test still fails (UCI conversion does not work).

--- ml2h5/tests.py
+++ ml2h5/tests.py
@@ -2,11 +2,19 @@ import random
 import unittest
 
 from converter.h5_csv import H5_CSV
+from converter.h5_arff import H5_ARFF
+from converter.h5_libsvm import H5_LibSVM
+from converter.h5_mat import H5_MAT
+from converter.h5_octave import H5_OCTAVE
+from converter.h5_rdata import H5_RData
+from converter.h5_uci import H5_UCI
 
 class TestConversion(unittest.TestCase):
 
     fixtures = {
         'csv': '../fixtures/test.csv',
+        'arff': '../fixtures/iris.arff',
+        'uci': '../fixtures/iris.data',
     }
     
 
@@ -17,6 +25,19 @@ class TestConversion(unittest.TestCase):
         converter = H5_CSV(self.fixtures['csv'])
         data = converter.read()
         self.assertEqual(data['data']['int3'][3], 4,
+                         'wrong first integer')
+
+    def test_arff2h5(self):
+        converter = H5_ARFF(self.fixtures['arff'])
+        data = converter.read()
+        self.assertEqual(data['data']['sepallength'][3], 4.6,
+                         'wrong first integer')
+
+    def test_uci(self):
+        converter = H5_UCI(self.fixtures['uci'])
+        data = converter.read()
+        print data
+        self.assertEqual(data['data']['sepallength'][3], 4.6,
                          'wrong first integer')
 
 if __name__ == '__main__':
